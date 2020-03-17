b9737f007e03cdc9fdf5d52ba795345c799922cb
Amith Koujalgi <amith.koujalgi@razorthink.com>
2020-03-17 20:00:55 +0530

    init

--- docker-compose/run.sh
+++ docker-compose/run.sh
@@ -5,5 +5,5 @@ docker rm `docker ps --no-trunc -aq`
 docker-compose down -v && \
     docker-compose rm -f -s && \
     docker-compose build --pull && \
-    docker-compose up -d --remove-orphans && \
+    docker-compose up --remove-orphans && \
     docker-compose logs -f
