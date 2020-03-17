a13c895a6ab46c5f02d69c6a6f58941eb15526f9
amithkoujalgi <amith.koujalgi@razorthink.com>
2020-03-17 10:19:54 +0530

    init

--- keycloak-realms-manager/src/main/java/com/amithkoujalgi/auth/RealmAdd.java
+++ keycloak-realms-manager/src/main/java/com/amithkoujalgi/auth/RealmAdd.java
@@ -1,8 +1,10 @@
 package com.amithkoujalgi.auth;
 
+import java.io.*;
+
 public class RealmAdd {
 
-	public static void main( String[] args )
+	public static void main( String[] args ) throws Exception
 	{
 		//https://www.n-k.de/2016/08/keycloak-admin-client.html
 		//https://www.keycloak.org/docs/latest/securing_apps/index.html#java-adapters
@@ -23,5 +25,48 @@ public class RealmAdd {
 		//		user.setUsername("johndoe");
 		//		//		user.set... // all the user attributes to set
 		//		kc.realm("demo").users().create(user);
+		createRealm("master");
+	}
+
+	public static String getTemplate() throws IOException
+	{
+		InputStream is = RealmAdd.class.getResourceAsStream("/keycloak-auth-server-config-template.json");
+		if( is == null )
+		{
+			throw new IllegalStateException("Not able to find the template file");
+		}
+		BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+		StringBuilder templateString = new StringBuilder();
+		String line;
+		while( (line = reader.readLine()) != null )
+		{
+			templateString.append(line);
+		}
+		reader.close();
+		return templateString.toString();
+	}
+
+	public static void createRealm( String realmName ) throws Exception
+	{
+		String REALMS_DIRECTORY =
+				System.getProperty("user.home") + File.separator + "keycloak" + File.separator + "realms";
+
+		File existingRealm = new File(REALMS_DIRECTORY + File.separator + realmName + "-keycloak.json");
+		if( existingRealm.exists() )
+		{
+			throw new Exception(String.format("Realm '%s' already exists.", realmName));
+		}
+		else
+		{
+			String template = getTemplate();
+			template = template.replace("{realm-name}", realmName);
+
+			File newRealm = new File(REALMS_DIRECTORY + File.separator + realmName + "-keycloak.json");
+			FileWriter fw = new FileWriter(newRealm);
+			fw.write(template);
+			fw.close();
+			System.out.println(String.format("Auth server config file for realm %s written to %s.", realmName,
+					newRealm.getAbsolutePath()));
+		}
 	}
 }
