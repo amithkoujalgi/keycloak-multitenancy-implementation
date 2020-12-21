73edc011e2a1dc88e9ad8b90ad0de6817618e582
amithkoujalgi <amith.koujalgi@razorthink.com>
2020-12-21 23:16:03 +0530

    init

--- docker-compose/run.sh
+++ docker-compose/run.sh
@@ -1,4 +1,43 @@
 #!/bin/bash
+function print_style() {
+  if [ "$2" == "info" ]; then
+    COLOR="96m"
+  elif [ "$2" == "success" ]; then
+    COLOR="92m"
+  elif [ "$2" == "warning" ]; then
+    COLOR="93m"
+  elif [ "$2" == "danger" ]; then
+    COLOR="91m"
+  else #default color
+    COLOR="0m"
+  fi
+  STARTCOLOR="\e[$COLOR"
+  ENDCOLOR="\e[0m"
+  printf "$STARTCOLOR%b$ENDCOLOR" "$1\n"
+}
+
+function usage() {
+    print_style "Invalid arguments!" "danger";
+    echo ""
+    print_style "Usage: " "info";
+    print_style "      ./run.sh [ip]" "info";
+    echo ""
+    print_style "Example:" "info";
+    print_style "      ./run.sh 192.168.0.50" "info";
+    echo ""
+    print_style "Use one of the entries from the detected IP addresses: " "info";
+    print_style "------------------------------------------------------" "info";
+    ip_list=`ifconfig | grep inet | grep netmask | grep broadcast | cut -d" " -f2`
+    print_style $ip_list "info";
+    echo ""
+    exit 1
+}
+
+if [[ $# -eq 0 ]] || [[ $# -ne 1 ]]; then
+	usage
+fi
+
+export IP_ADDRESS=$1
 
 echo "y" | docker container prune
 docker rm `docker ps --no-trunc -aq`
